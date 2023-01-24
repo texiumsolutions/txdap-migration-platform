310451e3a644d06a23713e84a0d8fd9818cad005
texiumsolutions <93979453+Arif11819@users.noreply.github.com>
2023-01-25 00:14:12 +0600

    added file after complete doc part

--- src/Home/NavbarRoutes/Injection/Injection.js
+++ src/Home/NavbarRoutes/Injection/Injection.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Home from '../../Home';
 
 const Injection = () => {
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit = (data) => {
+
+        console.log(data);
+
+
+    };
+
     return (
         <div>
             <Home></Home>
-            <h1>This is Injection Page</h1>
+            <div className='flex justify-between'>
+                <div className='pl-24 pt-10'>
+                    <h1 className='text-3xl pl-16 text-start pt-6'>Document</h1>
+
+                    <div className='pt-10'>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className='card w-80 h-36 bg-base-100 shadow-xl pl-6'>
+                                <label className='text-sm flex justify-start items-start pt-8 pb-2' htmlFor="doc">Search types of doc*</label>
+                                <div className='flex justify-start'>
+                                    <select {...register("doc")} className='w-[200px] flex justify-start text-sm border border-slate-500 rounded-sm bg-white' name="doc" >
+                                        <option value="Technology expert">Technology expert</option>
+                                        <option value="HR Manager">HR Manager</option>
+                                        <option value="Product Manager">Product Manager</option>
+                                        <option value="Product Analist">Product Analist</option>
+                                    </select>
+                                    <input type="submit" value='Submit' className='text-[10px] text-black px-3 py-1 ml-2 border border-black rounded-sm cursor-pointer' />
+                                </div>
+                            </div>
+                            <div className='pl-[270px] pt-10'>
+                                <input type="submit" value='Start' className='text-md text-black px-20 py-2 border border-black rounded-md cursor-pointer' />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className='pr-40 pt-10'>
+                    <h1 className='text-3xl pr-16 pt-6'>File Upload</h1>
+                </div>
+            </div>
+
         </div>
     );
 };
