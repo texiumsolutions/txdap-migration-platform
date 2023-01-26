cbd109685b049c762ea87cd9a3e1cc244035047b
sheikhsumaya <sheikhsumaya622@gmail.com>
2023-01-26 17:21:24 +0600

    active route

--- src/Home/NavbarRoutes/Extraction/Extraction.js
+++ src/Home/NavbarRoutes/Extraction/Extraction.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {NavLink } from 'react-router-dom';
 import Home from '../../Home';
+import './Extraction.css';
 
 const Extraction = () => {
     return (
         <div>
             <Home></Home>
-            <ul class="menu menu-horizontal bg-gray-100 w-screen px-10 my-4 py-4 rounded-box">
-                <li><Link to="/eprofile">Extraction Profile</Link></li>
-                <li><Link to="/informations">Run Profile</Link></li>
+            <ul class=" flex bg-gray-100 w-screen px-10 my-4 py-4 rounded-box">
+                <li><NavLink exact activeClassName="active" className='extraction-navbar' to="/eprofile">Extraction Profile</NavLink></li>
+                <li><NavLink activeClassName="active" className='extraction-navbar' to="/informations">Run Profile</NavLink></li>
             </ul>
         </div>
     );
