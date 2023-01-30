ff31a230392f85e7bc7bf89ccbb935dd27423dc2
texiumsolutions <93979453+Arif11819@users.noreply.github.com>
2023-01-30 20:28:40 +0600

    added file now

--- src/Home/NavbarRoutes/Injection/DocumentInfo.js
+++ src/Home/NavbarRoutes/Injection/DocumentInfo.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast, ToastContainer } from 'react-toastify';
 import useInformation from '../../../hooks/useInformation';
 import Home from '../../Home';
+import Circle from './Circle';
 
 import Injection from './Injection';
-import Progressbar2 from './Progressbar2';
+
+import '././Progress.css'
 
 const DocumentInfo = () => {
+
+
+    const [pcircle] = useState(5);
+    const [active, setActive] = useState(0);
+    const [width, setWidth] = useState(0);
+    useEffect(() => {
+        setWidth((100 / (pcircle - 1)) * active);
+    }, [pcircle, active]);
+
+    const arr = [];
+    for (let i = 0; i < pcircle; i++) {
+        arr.push(
+            <Circle className={i <= active ? "pcircle active" : "pcircle"} key={i}>
+                {i}
+            </Circle>
+        );
+    }
+
+    const [circles, setCircles] = useState([]);
+
+    useEffect(() => {
+        fetch("circleData.json")
+            .then((res) => res.json())
+            .then((data) => setCircles(data));
+    }, []);
+
     const [information] = useInformation();
     const { register, handleSubmit, reset } = useForm();
 
@@ -33,24 +61,44 @@ const DocumentInfo = () => {
             <div className='card w-[1400px] h-[600px] mt-12 ml-16 bg-base-100 shadow-xl drop-shadow-2xl'>
                 <Injection></Injection>
 
-                <div className='pt-16'>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div className='pl-6'>
-                            <label className='text-md flex justify-start items-start pt-8 pb-2' htmlFor="doc">Search types of doc*</label>
-                            <div className='flex justify-start'>
-                                <select {...register("doc")} className='w-[200px] flex justify-start text-md border border-slate-500 rounded-sm bg-white' name="doc" >
-                                    {
-                                        information.map(info => <option
-                                            key={info._id}
-                                            info={info}
-                                        >{info.name}</option>)
-                                    }
-                                </select>
-                                <input type="submit" value='Submit' className='text-[16px] text-black px-3 py-1 ml-2 border border-black rounded-sm cursor-pointer' />
+                <div className='flex justify-between'>
+                    <div className='pt-16'>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className='pl-6'>
+                                <label className='text-md flex justify-start items-start pt-8 pb-2' htmlFor="doc">Search types of doc*</label>
+                                <div className='flex justify-start'>
+                                    <select {...register("doc")} className='w-[200px] flex justify-start text-md border border-slate-500 rounded-sm bg-white' name="doc" >
+                                        {
+                                            information.map(info => <option
+                                                key={info._id}
+                                                info={info}
+                                            >{info.name}</option>)
+                                        }
+                                    </select>
+                                    <input onClick={() => {
+                                        active >= pcircle ? setActive(pcircle) : setActive(active + 1);
+                                    }} type="submit" value='Submit' className='text-[16px] text-black px-3 py-1 ml-2 border border-black rounded-sm cursor-pointer' />
+                                </div>
                             </div>
+                        </form>
+                    </div>
+                    <div className="pcontainer">
+                        <div className="pcontent">
+                            <div className="pprogressbar">
+                                <div
+                                    className="pprogress"
+                                    style={{
+                                        width: width + "%",
+                                    }}
+                                ></div>
+                                {arr}
+                            </div>
+                            {circles.map((circle) => (
+                                <Circle key={circle.id} circle={circle}></Circle>
+                            ))}
+
                         </div>
-                        <Progressbar2></Progressbar2>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Home/NavbarRoutes/Injection/Progressbar2.js
+++ src/Home/NavbarRoutes/Injection/Progressbar2.js
@@ -1,69 +1,68 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import Circle from "./Circle";
-import "./Progress.css";
+// import React from "react";
+// import { useEffect } from "react";
+// import { useState } from "react";
+// import "./Progress.css";
 
-const Progressbar2 = () => {
-    const [pcircle] = useState(5);
-    const [active, setActive] = useState(0);
-    const [width, setWidth] = useState(0);
-    useEffect(() => {
-        setWidth((100 / (pcircle - 1)) * active);
-    }, [pcircle, active]);
+// const Progressbar2 = () => {
+//     const [pcircle] = useState(5);
+//     const [active, setActive] = useState(0);
+//     const [width, setWidth] = useState(0);
+//     useEffect(() => {
+//         setWidth((100 / (pcircle - 1)) * active);
+//     }, [pcircle, active]);
 
-    const arr = [];
-    for (let i = 0; i < pcircle; i++) {
-        arr.push(
-            <Circle className={i <= active ? "pcircle active" : "pcircle"} key={i}>
-                {i}
-            </Circle>
-        );
-    }
+//     const arr = [];
+//     for (let i = 0; i < pcircle; i++) {
+//         arr.push(
+//             <Circle className={i <= active ? "pcircle active" : "pcircle"} key={i}>
+//                 {i}
+//             </Circle>
+//         );
+//     }
 
-    const [circles, setCircles] = useState([]);
+//     const [circles, setCircles] = useState([]);
 
-    useEffect(() => {
-        fetch("circleData.json")
-            .then((res) => res.json())
-            .then((data) => setCircles(data));
-    }, []);
-    return (
-        <div className="pcontainer">
-            <div className="pcontent">
-                <div className="pprogressbar">
-                    <div
-                        className="pprogress"
-                        style={{
-                            width: width + "%",
-                        }}
-                    ></div>
-                    {arr}
-                </div>
-                {circles.map((circle) => (
-                    <Circle key={circle.id} circle={circle}></Circle>
-                ))}
-                <div className="pbutton">
-                    <button
-                        className="prev btnn"
-                        onClick={() => {
-                            active <= 0 ? setActive(0) : setActive(active - 1);
-                        }}
-                    >
-                        Prev
-                    </button>
-                    <button
-                        className="next btnn"
-                        onClick={() => {
-                            active >= pcircle ? setActive(pcircle) : setActive(active + 1);
-                        }}
-                    >
-                        Next
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
-};
+//     useEffect(() => {
+//         fetch("circleData.json")
+//             .then((res) => res.json())
+//             .then((data) => setCircles(data));
+//     }, []);
+//     return (
+//         <div className="pcontainer">
+//             <div className="pcontent">
+//                 <div className="pprogressbar">
+//                     <div
+//                         className="pprogress"
+//                         style={{
+//                             width: width + "%",
+//                         }}
+//                     ></div>
+//                     {arr}
+//                 </div>
+//                 {circles.map((circle) => (
+//                     <Circle key={circle.id} circle={circle}></Circle>
+//                 ))}
+//                 <div className="pbutton">
+//                     <button
+//                         className="prev btnn"
+//                         onClick={() => {
+//                             active <= 0 ? setActive(0) : setActive(active - 1);
+//                         }}
+//                     >
+//                         Prev
+//                     </button>
+//                     <button
+//                         className="next btnn"
+//                         onClick={() => {
+//                             active >= pcircle ? setActive(pcircle) : setActive(active + 1);
+//                         }}
+//                     >
+//                         Next
+//                     </button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
 
-export default Progressbar2;
+// export default Progressbar2;
