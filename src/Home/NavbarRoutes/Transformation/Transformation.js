56c7487539277b140c8c72612f18eae22b4207bf
sheikhsumaya <sheikhsumaya622@gmail.com>
2023-02-06 07:42:41 +0600

    updated

--- src/Home/NavbarRoutes/Transformation/Transformation.js
+++ src/Home/NavbarRoutes/Transformation/Transformation.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { toast } from "react-toastify";
-import useInformation from '../../../hooks/useInformation';
-import Home from '../../Home';
+import React from "react";
+import { useForm } from "react-hook-form";
+import useInformation from "../../../hooks/useInformation";
+import Home from "../../Home";
+import "./Transformation.css";
 
 const Transformation = () => {
-    const [informations] = useInformation();
-    const { register, handleSubmit, reset } = useForm();
+  const [informations] = useInformation();
+  const { register, handleSubmit, reset } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -22,59 +22,75 @@ const Transformation = () => {
       .then((res) => res.json())
       .then((inserted) => {
         if (inserted.insertedId) {
-          toast.success("Added New Product Successfully");
+          // toast.success("Added New Product Successfully");
 
           reset();
         } else {
-          toast.error("Failed add to the data");
+          // toast.error("Failed add to the data");
         }
       });
   };
-    return (
-        <div>
-            <Home></Home>
-            <p className="text-2xl p-6">Action Mapping </p>
-            <div class="overflow-x-auto">
-  <table class="table w-full">
-    {/* <!-- head --> */}
-    <thead>
-        
-      <tr>
-        <th>Source Key</th>
-        <th>Target Key</th>
-        <th>Data Type</th>
-      </tr>
-    </thead>
-    <tbody style={{
-        border: "1px solid black",
-    }}>   
-      
-      <td className='px-20'>
-      {informations.map((information) => (
-                  <label className="label justify-start" key={information._id} information={information}>
+
+  return (
+    <div>
+      <Home></Home>
+      <p className="text-2xl p-6">Action Mapping </p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div class="overflow-x-auto">
+          <table class="table w-full">
+            {/* <!-- head --> */}
+            <thead>
+              <tr>
+                <th>Source Key</th>
+                <th>Target Key</th>
+                <th>Data Type</th>
+              </tr>
+            </thead>
+            <tbody
+              style={{
+                border: "1px solid black",
+              }}
+            >
+              <td className="px-20">
+                {informations.map((information) => (
+                  <label
+                    className="label justify-start"
+                    key={information._id}
+                    information={information}
+                  >
                     {information.name}
-                    </label>
+                  </label>
                 ))}
-      </td>   
-      <td>
-      {informations.map((information) => (
-                  <label className="label justify-start" key={information._id} information={information}>
-                    <input 
-                    style={{
+              </td>
+              <td>
+                {informations.map((information) => (
+                  <label
+                    className="label justify-start"
+                    key={information._id}
+                    information={information}
+                  >
+                    <input
+                      style={{
                         border: "1px solid black",
-                    }}
-                type="text"
-                placeholder="Update here"
-                required
-              />
-                    </label>
+                      }}
+                      type="text"
+                      defaultValue={information.name}
+                      required
+                    />
+                  </label>
                 ))}
-      </td>     
-    </tbody>
-  </table>
-</div>
+              </td>
+            </tbody>
+          </table>
+          <input
+              className="btn transformation_button bg-blue-700 m-8"
+              type="submit"
+              value="Create Mapping"
+            />
+        </div>
+      </form>
     </div>
-    );
+  );
 };
 
 export default Transformation;
